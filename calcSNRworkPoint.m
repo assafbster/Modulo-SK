16f315b367d6636b9b2c0cf7769687b67889630b
assafbster <68911987+assafbster@users.noreply.github.com>
2021-07-15 09:08:09 +0300

    Add files via upload

--- calcSNRworkPoint.m
+++ calcSNRworkPoint.m
@@ -5,13 +5,14 @@ Dsnr = 10^(DsnrdB/10);
 PAMcapGap = 1/3*(qfuncinv(Petarget/2))^2;
 PAMcapGapdB = 10*log10(PAMcapGap);
 snrShannondB = 10*log10(2^(2*R)-1);
-
+snrdBmin = snrShannondB+min(0,PAMcapGapdB);
+snrdBmax = snrShannondB+max(9,PAMcapGapdB);
 if (N==1)
     CapGapdB = PAMcapGapdB;
 else
     if  isinf(DsnrdB) % noiseleff feedback. use classic SK
         Gamma0SK = 1/3*(qfuncinv(Petarget/2))^2;
-        snrVecdB = snrShannondB:0.01:snrShannondB+PAMcapGapdB;
+        snrVecdB = snrdBmin:0.01:snrdBmax;
         for ii = 1:length(snrVecdB)
             SNR = 10^(snrVecdB(ii)/10);
             snrn = SNR*(1+SNR)^(N-1);
@@ -28,7 +29,7 @@ else
         L = 1/3*(qfuncinv(pm/2))^2;
         Psi3 = 1+3/2*L*pm*(N-1-2/N);
         Psi1 = 1+Psi3*L/Dsnr;
-        snrVecdB = (snrShannondB-3):0.01:(snrShannondB+PAMcapGapdB);
+        snrVecdB = snrdBmin:0.01:snrdBmax;
         pointFound = 0;
         for ii = 1:length(snrVecdB)
             SNR = 10^(snrVecdB(ii)/10);
